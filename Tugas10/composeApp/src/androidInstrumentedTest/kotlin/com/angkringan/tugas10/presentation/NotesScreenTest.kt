70802bca466ec47f24e37cd0d7d4751f63290122
Dakim777 <daffa8692@gmail.com>
2026-05-13 13:41:31 +0700

    fix eror NotesScreenTest

--- Tugas10/composeApp/src/androidInstrumentedTest/kotlin/com/angkringan/tugas10/presentation/NotesScreenTest.kt
+++ Tugas10/composeApp/src/androidInstrumentedTest/kotlin/com/angkringan/tugas10/presentation/NotesScreenTest.kt
@@ -4,10 +4,13 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.angkringan.tugas10.data.Note
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class NotesScreenTest {
 
     // Rule wajib untuk menjalankan testing UI di Compose
